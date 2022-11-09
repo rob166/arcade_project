df48e48e8dfbffdd896a6983285d532fe09a7864
Robert Rosenzweig <robgrosenzweig@gmail.com>
2022-11-08 21:03:28 -0500

    Working solution with comments

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Variable declarations from HTML
+
 let playerOneName = document.getElementById('player_one');
 let playerTwoName = document.getElementById('player_two');
 let playerOneDisplay = document.getElementById('player_one_name_display');
@@ -11,93 +13,67 @@ let playerTwoPoints = document.getElementById('player_two_points');
 let buttonNewGame = document.getElementById('reset');
 let dataCell = document.getElementsByClassName('data_cell_class');
 
+// Initialze counter variables for each player to track scores
+
 let playerXCount = 0;
 let playerOCount = 0;
+
+// Initialize variable to track which player's turn it is
+
 let playerTurn = 'X';
+
+// Initialize the array to keep track of which data cells are selected
+
 let gameArray = ['', '', '', '', '', '', '', '', ''];
 
-
-buttonPlayerOne.addEventListener('click', function() {
-  playerOneDisplay.innerHTML = `${playerOneName.value}, you will play as 'X' and go first`
-  playerOneName.value = "";
-  buttonPlayerOne.disabled = true;
-  return;
-}
-);
-
-buttonPlayerTwo.addEventListener('click', function() {
-  playerTwoDisplay.innerHTML = `${playerTwoName.value}, you will play as 'O' and go second`
-  playerTwoName.value = "";
-  buttonPlayerTwo.disabled = true;
-  return;
-}
-);
-
-gameProgress.innerHTML = `it is ${playerTurn}'s turn`;
-
-tableBoard.addEventListener('click', clickDataCell);
+//Initialize an array to determine if the game ends in a draw
 
 let filledArray = [];
 
-function clickDataCell (event) {
-  let targetIdValue = event.target.id;
+// Show which player's turn it is
 
-  
-  if (gameArray[targetIdValue] !=='') {
-    return;
+gameProgress.innerHTML = `it is ${playerTurn}'s turn`;
 
-  } else {
-    gameArray[targetIdValue] = playerTurn;
-    
-   
-    event.target.innerHTML = playerTurn;
+// Helper Functions
 
-    filledArray += gameArray[targetIdValue];
-    
-    if (filledArray.length >=9) {
-      gameProgress.innerHTML = `IT IS A DRAW!  No points, try again!`;
-      setTimeout(newGame, 1000);
-      return ;
-    };
-  
-    if (checkIfWon()) {
+// Function to switch between players for each turn
+// Uses a ternanry operator instead of 'if-else' statements
 
-      gameProgress.innerHTML = `${playerTurn} HAS WON!  You get a point!`;
-      
-      setTimeout(newGame, 1000);
-
-      if (playerTurn == 'X') {
-        playerXCount = ++playerXCount
-        playerOnePoints.innerHTML = `${playerTurn} has ${playerXCount} points`;
-        return;
-
-      } else {
-        playerOCount = ++playerOCount
-        playerTwoPoints.innerHTML = `${playerTurn} has ${playerOCount} points`;
-        return;
-      };
-    
-    };
-    
-  };
-switchPlayer();
-return;
+function switchPlayer(){
+  playerTurn = playerTurn === 'X' ? 'O' : 'X';
+  gameProgress.innerHTML = `it is ${playerTurn}'s turn`;
+  return;
 };
 
+// Function to reset the board
+
 function newGame() {
+  
+// Iterate through all 9 data cells to reset each to empty
+
   for (i = 0; i<= 8; ++i){
     dataCell[i].innerHTML = '';
   };
 
+// Reset the array used to keep track of which data cells have been selected
+
   gameArray = ['', '', '', '', '', '', '', '', ''];
+
+// Reset 'X' to start the game  
+
   playerTurn = 'X';
   gameProgress.innerHTML = `it is ${playerTurn}'s turn`;
+
+ // Reset the array used to check for a draw
+
   filledArray = [];
   return;
 };
 
-buttonNewGame.addEventListener('click', newGame);
-   
+// Function to check if a player has won
+// Looks to see if the current player's turn has completed one of 
+// the winning combinations of row, column, or diagonal data cells
+
 function checkIfWon() {
   if (gameArray[0] === playerTurn) {
     if (gameArray[1] === playerTurn && gameArray[2] === playerTurn) {
@@ -134,14 +110,105 @@ function checkIfWon() {
     return true;
     }
   }
-
 };
 
-function switchPlayer(){
-  playerTurn = playerTurn === 'X' ? 'O' : 'X';
-  gameProgress.innerHTML = `it is ${playerTurn}'s turn`;
+// Main function to determine game play when a data cell is clicked
+
+function clickDataCell (event) {
+  let targetIdValue = event.target.id;
+
+// Deterine if data cell already has been selected (not empty), if not 
+// claim it for the current player
+
+  if (gameArray[targetIdValue] !=='') {
+    return;
+
+  } else {
+    gameArray[targetIdValue] = playerTurn;
+    
+// Add the curent player's symbol to the data cell
+
+    event.target.innerHTML = playerTurn;
+
+// Add the claimed data cell to the array used to determine a draw    
+
+    filledArray += gameArray[targetIdValue];
+
+// If the draw array contains 9 values, the game ends in a draw
+// Wait one second and reset the board    
+    
+    if (filledArray.length >=9) {
+      gameProgress.innerHTML = `IT IS A DRAW!  No points, try again!`;
+      setTimeout(newGame, 1000);
+      return ;
+    };
+
+// Check the clicked cell to see if it completes a winning game
+// and announce that to the players if true
+
+    if (checkIfWon()) {
+
+      gameProgress.innerHTML = `${playerTurn} HAS WON!  You get a point!`;
+
+// Wait one second before resetting the game board to continue play      
+      
+      setTimeout(newGame, 1000);
+
+// Add a point to the winning player's total after the  board reset to 
+// maintain the score cumulatively      
+
+      if (playerTurn == 'X') {
+        playerXCount = ++playerXCount
+        playerOnePoints.innerHTML = `${playerTurn} has ${playerXCount} points`;
+        return;
+
+      } else {
+        playerOCount = ++playerOCount
+        playerTwoPoints.innerHTML = `${playerTurn} has ${playerOCount} points`;
+        return;
+      };
+    
+    };
+    
+  };
+
+// Switch the players after evaluating each move for win or draw
+// to continue play
+
+switchPlayer();
+return;
+};
+
+// Event Listeners
+
+// Listener for data cell activation
+
+tableBoard.addEventListener('click', clickDataCell);
+
+// Listener for adding Player One's name to screen and assign to 'X'
+
+buttonPlayerOne.addEventListener('click', function() {
+  playerOneDisplay.innerHTML = `${playerOneName.value}, you will play as 'X' and go first`
+  
+// Clear input box after entering name and disable button
+
+  playerOneName.value = "";
+  buttonPlayerOne.disabled = true;
   return;
+}
+);
 
-};
+// Listener for adding Player Two's name to screen and assign to 'O'
 
+buttonPlayerTwo.addEventListener('click', function() {
+  playerTwoDisplay.innerHTML = `${playerTwoName.value}, you will play as 'O' and go second`
+  playerTwoName.value = "";
+  buttonPlayerTwo.disabled = true;
+  return;
+}
+);
+
+// Listener to reset the board
+
+buttonNewGame.addEventListener('click', newGame);
 
